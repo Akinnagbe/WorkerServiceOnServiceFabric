04e7d2dc746c7528990fc9effc3a008af712f4d9
Olamide james <olamidejames007@gmail.com>
2020-09-08 18:44:17 +0100

    * .dockerignore: initial commit
    * .gitignore:
    * docker-compose.yml:
    * docker-compose.dcproj:
    * docker-compose.override.yml:
    * WorkerServiceOnServiceFabric.sln:
    * Worker.cs:
    * Dockerfile:
    * Program.cs:
    * appsettings.json:
    * appsettings.Development.json:
    * launchSettings.json:
    * WorkerServiceOnServiceFabric.csproj:

---
+++ WorkerServiceOnServiceFabric/Worker.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace WorkerServiceOnServiceFabric
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+
+            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+
+            var connectionFactory = new ConnectionFactory() { UserName = "guest", Password = "guest", Port = 5672, VirtualHost = "/", HostName = "nipsimulator.westeurope.azurecontainer.io"};
+            IConnection connection = connectionFactory.CreateConnection();
+            IModel channel = connection.CreateModel();
+            channel.BasicQos(0, 1, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received {0}", message);
+
+                _logger.LogInformation(" [x] Received {0}", message);
+
+                // Note: it is possible to access the channel via
+                //       ((EventingBasicConsumer)sender).Model here
+                channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+            };
+            channel.BasicConsume(queue: "nipinwardsimulator",
+                                 autoAck: false,
+                                 consumer: consumer);
+
+
+            //while (!stoppingToken.IsCancellationRequested)
+            //{
+            //    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            //    await Task.Delay(1000, stoppingToken);
+            //}
+        }
+
+        
+    }
+}
